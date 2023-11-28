df908a6c62a0bddde0123fa8e926072683cb1886
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-28 14:39:05 +0100

    feat: milestone 1

--- js/main.js
+++ js/main.js
@@ -5,8 +5,13 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
+        objs:[{text: 'text1', done: false }, {text: 'text2', done: false }, {text: 'text3', done: true }, {text: 'text4', done: false }, {text: 'text5', done: false }, {text: 'text6', done: true },]
         
       }
     },
-    methods: {},
+    methods: {
+        isDone(obj){
+            return obj.done;
+        }
+    },
   }).mount('#app')
