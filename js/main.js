1d53dff082b5f5485b54a0386851966a6f2daba9
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-28 14:57:25 +0100

    feat: milestone 2

--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        objs:[{text: 'text1', done: false }, {text: 'text2', done: false }, {text: 'text3', done: true }, {text: 'text4', done: false }, {text: 'text5', done: false }, {text: 'text6', done: true },]
+        objs:[{text: 'Text1', done: false }, {text: 'Text2', done: false }, {text: 'Text3', done: true }, {text: 'Text4', done: false }, {text: 'Text5', done: false }, {text: 'Text6', done: true },]
         
       }
     },
     methods: {
         isDone(obj){
             return obj.done;
-        }
+        },
+
+        deleteObj(index){
+            this.objs.splice(index, 1);
+        },
     },
   }).mount('#app')
